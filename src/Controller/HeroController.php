0045a2bc55b058b06ebcee9b65f48e1aa3e39097
MedicArwen <45285123+MedicArwen@users.noreply.github.com>
2021-07-19 17:22:48 +0200

    quelques ajouts
    installations et configuratuin du projet, création controller...

---
+++ src/Controller/HeroController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Controller;
+
+use App\Services\HeroService;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HeroController extends AbstractController
+{
+    /**
+     * @Route("/hero", name="hero")
+     */
+    public function index(): Response
+    {
+        return $this->render('hero/index.html.twig', [
+            'controller_name' => 'HeroController',
+        ]);
+    }
+    /**
+     * @Route("/hero/list", name="liste_hero")
+     */
+    public function list(HeroService $heroService): Response
+    {
+        $listeHeros =$heroService->getList();
+        return $this->render('hero/list.html.twig',
+        [
+            'heroList'=>$listeHeros
+        ]
+        );
+    }
+
+
+}
---
+++ src/Entity/Hero.php
@@ -0,0 +1,117 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\HeroRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=HeroRepository::class)
+ */
+class Hero
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $firstName;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $pseudo;
+
+    /**
+     * @ORM\Column(type="boolean")
+     */
+    private $isEvil;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $description;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getFirstName(): ?string
+    {
+        return $this->firstName;
+    }
+
+    public function setFirstName(string $firstName): self
+    {
+        $this->firstName = $firstName;
+
+        return $this;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getPseudo(): ?string
+    {
+        return $this->pseudo;
+    }
+
+    public function setPseudo(string $pseudo): self
+    {
+        $this->pseudo = $pseudo;
+
+        return $this;
+    }
+
+    public function getIsEvil(): ?bool
+    {
+        return $this->isEvil;
+    }
+
+    public function setIsEvil(bool $isEvil): self
+    {
+        $this->isEvil = $isEvil;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+    public function __construct($pName,$pFirstName,$pIsEvil,$pPseudo,$pDescription)
+    {
+        $this->name = $pName;
+        $this->firstName = $pFirstName;
+        $this->isEvil = $pIsEvil;
+        $this->pseudo = $pPseudo;
+        $this->description = $pDescription;
+    }
+}
